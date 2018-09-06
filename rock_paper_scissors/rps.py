28c687b009c1506f6a913d6d65c0dc0957548e81
Vu Cao <cdhvu@yahoo.com>
2018-09-06 15:16:24 -0700

    bug fixed

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,14 +2,27 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    rps = ['rock', 'paper', 'scissors']
+    outcomes = []
+
+    def rounds(count, round = [] ):
+        if count == 0:
+            outcomes.append(round)
+            return
+        for i in rps:
+            rounds(count-1, round + [i])
+
+    rounds(n, [])
+    return outcomes
+
 
 if __name__ == "__main__":
-  # Test out your implementation from the command line
-  # with `python rps.py [n]` with different n values
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    # Test out your implementation from the command line
+    # with `python rps.py [n]` with different n values
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
